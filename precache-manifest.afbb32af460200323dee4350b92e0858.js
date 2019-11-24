dd137ff40c3c6155df52a4c9ded4ca14311f87ea
Osama Imadi <osamaimadi@yahoo.com>
2019-11-24 20:54:41 +0500

    Updates

---
+++ precache-manifest.afbb32af460200323dee4350b92e0858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad9d8721d58ae3c4b9be7bf13fcb075",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "9ebe4efe4136c8331743",
+    "url": "/react-monsters/static/css/main.db5d8aeb.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/react-monsters/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "9ebe4efe4136c8331743",
+    "url": "/react-monsters/static/js/main.d8c36d33.chunk.js"
+  },
+  {
+    "revision": "a7a03faef100c6e7d8a5",
+    "url": "/react-monsters/static/js/runtime-main.3bb2187e.js"
+  }
+]);
